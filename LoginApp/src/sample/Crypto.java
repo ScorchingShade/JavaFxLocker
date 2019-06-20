d9fdb1548f2785ddc5a0124f01aa4900aa13a1bb
Ankush Sharma <ankushors789@gmail.com>
2019-06-20 03:36:25 -0700

    added zip and unzip, handled exceptions, Ready to code for encryption and decryption

--- LoginApp/src/sample/Crypto.java
+++ LoginApp/src/sample/Crypto.java
@@ -1,7 +1,148 @@
 package sample;
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.KeySpec;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 public class Crypto {
     //doing cryptography
     //encrypting folder by zipping it
     //decrypting folder and unzipping it
+
+    public static void pack(String sourceDirPath, String zipFilePath) throws IOException {
+        Path p = Files.createFile(Paths.get(zipFilePath));
+        try (ZipOutputStream zs = new ZipOutputStream(Files.newOutputStream(p))) {
+            Path pp = Paths.get(sourceDirPath);
+            Files.walk(pp)
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        ZipEntry zipEntry = new ZipEntry(pp.relativize(path).toString());
+                        try {
+                            zs.putNextEntry(zipEntry);
+                            Files.copy(path, zs);
+                            zs.closeEntry();
+                        } catch (IOException e) {
+                            System.err.println(e);
+                        }
+                    });
+        }
+    }
+
+    private static final int BUFFER_SIZE = 4096;
+
+    public void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    public void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+
+
+    public static boolean deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDirectory(children[i]);
+                if (!success) {
+                    return false;
+                }
+            }
+        } // either file or an empty directory
+         System.out.println("removing file or directory : " + dir.getName());
+        return dir.delete();
+    }
+
+
+    public void encrypsa(File inFile,File outFile, String password) throws IOException, IllegalBlockSizeException,BadPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidParameterSpecException,InvalidKeyException {
+        byte[] salt = new byte[8];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(salt);
+        FileOutputStream saltOutFile = new FileOutputStream("salt.enc");
+        saltOutFile.write(salt);
+        saltOutFile.close();
+
+        SecretKeyFactory factory = SecretKeyFactory
+                .getInstance("PBKDF2WithHmacSHA1");
+        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
+                256);
+        SecretKey secretKey = factory.generateSecret(keySpec);
+        SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+        //
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        AlgorithmParameters params = cipher.getParameters();
+
+        // iv adds randomness to the text and just makes the mechanism more
+        // secure
+        // used while initializing the cipher
+        // file to store the iv
+        FileOutputStream ivOutFile = new FileOutputStream("iv.enc");
+        byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+        ivOutFile.write(iv);
+        ivOutFile.close();
+
+        //file encryption
+        byte[] input = new byte[64];
+        int bytesRead;
+
+        Reader read=new InputStreamReader();
+        while ((bytesRead = inFile.inputStreamReader  read(input)) != -1) {
+            byte[] output = cipher.update(input, 0, bytesRead);
+            if (output != null)
+                outFile.write(output);
+        }
+
+        byte[] output = cipher.doFinal();
+        if (output != null)
+            outFile.write(output);
+
+        inFile.close();
+        outFile.flush();
+        outFile.close();
+
+        System.out.println("File Encrypted.");
+    }
 }
--- LoginApp/src/sample/Home.java
+++ LoginApp/src/sample/Home.java
@@ -1,13 +1,11 @@
 package sample;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -20,6 +18,9 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class Home {
+    String source,zipPath = "";
+    File file=null;
+
     @FXML
     private javafx.scene.control.Button closeButton;
     @FXML
@@ -29,19 +30,17 @@ public class Home {
     @FXML
     private ImageView image;
     @FXML
-    private javafx.scene.control.Button browse;
+    private javafx.scene.control.Button browse, decrypt;
+    private javafx.scene.control.Button encrypt;
+
     @FXML
     private javafx.scene.control.TextField bro;
     @FXML
-    public javafx.scene.control.Label info,info1;
-
-
-
-
+    public javafx.scene.control.Label info, info1;
 
 
     @FXML
-    private void closeButtonAction(){
+    private void closeButtonAction() {
         // get a handle to the stage
 
 
@@ -55,13 +54,10 @@ public class Home {
         alert.setContentText("Are you sure you want to exit?");
 
         alert.getButtonTypes().clear();
-        alert.getButtonTypes().addAll(yes,no);
-
+        alert.getButtonTypes().addAll(yes, no);
 
 
         Optional<ButtonType> option = alert.showAndWait();
-
-
 
 
         if (option.get() == yes) {
@@ -74,7 +70,7 @@ public class Home {
 
 
     @FXML
-    private void SignOut(){
+    private void SignOut() {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Sign Out");
@@ -86,23 +82,20 @@ public class Home {
         alert.setContentText("Are you sure you want to logout?");
 
         alert.getButtonTypes().clear();
-        alert.getButtonTypes().addAll(yes,no);
-
+        alert.getButtonTypes().addAll(yes, no);
 
 
         Optional<ButtonType> option = alert.showAndWait();
-
-
 
 
         if (option.get() == yes) {
             image.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
-                    try{
+                    try {
                         GridPane Hp1 = FXMLLoader.load(this.getClass().getResource("/sample/sample.fxml"));
-                        Hpane.getChildren().setAll(Hp1);}
-                    catch (IOException i){
+                        Hpane.getChildren().setAll(Hp1);
+                    } catch (IOException i) {
                         i.printStackTrace();
                     }
                 }
@@ -110,27 +103,71 @@ public class Home {
         }
 
 
+    }
+
+    @FXML
+    private void Browse(ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+
+        Stage stage = (Stage) Hpane.getScene().getWindow();
+
+        this.file = directoryChooser.showDialog(stage);
+
+        if (file != null) {
+            System.out.println("Path: " + file.getAbsolutePath());
+            bro.setText(file.getAbsolutePath());
+            info1.setText("Ready for Operation!");
+
+            this.source = file.getAbsolutePath();
+
+        }
 
     }
 
     @FXML
-    private void Browse (ActionEvent event){
-        final DirectoryChooser directoryChooser=new DirectoryChooser();
+    private void Encrypt(ActionEvent event) {
+        Crypto c1 = new Crypto();
+        boolean z=false;
 
-        Stage stage= (Stage) Hpane.getScene().getWindow();
-
-        File file =directoryChooser.showDialog(stage);
-
-        if(file!=null){
-            System.out.println("Path: "+file.getAbsolutePath());
-            bro.setText(file.getAbsolutePath());
-            info1.setText("Ready for Operation!");
+        if (source.equals(null)) {
+            info1.setText("Can't encrypt without a path");
+        } else {
+            zipPath = source + "zipper.zip";
+            System.out.println(zipPath);
+            try {
+                c1.pack(source, zipPath);
+                z=c1.deleteDirectory(file);
+                info1.setText("Encrypted");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
+
+    @FXML
+    private void Decrypt (ActionEvent event){
+        Crypto c1 = new Crypto();
+        System.out.println(source);
+        boolean z=true;
+        if (zipPath.equals(null)) {
+            info1.setText("Can't decrypt without a path");
+        } else {
+
+            try {
+                c1.unzip(zipPath,source);
+                file=new File(zipPath);
+                z=c1.deleteDirectory(file);
+                info1.setText("Decrypted");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 
 
 
 
 }
+
